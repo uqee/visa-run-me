6b5d9b8457d0fadf8aad90adf3257f15f396aa5f
Denis Zhbankov <denis.zhbankov@gmail.com>
2022-04-25 00:12:00 +0300

    * alpha: migrations

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # visa-run-me
 
-2202-04-23
-
-USE CASES
+## Use cases
 
     нужен визаран
       пассажир
@@ -18,84 +16,9 @@ USE CASES
         еду если наберется рейс
           могу взять разные машины
 
-DATABASE
+## Logs
 
-    Countries
-      Id:Ref
-      Name:Name
-      ChangeTimestamp:ChangeTimestamp
-
-    Places
-      Id:Ref
-      CountryId:Ref
-      Name:Name
-      Position:LatLong?
-      ChangeTimestamp:ChangeTimestamp
-
-    Persons - private
-      Id:Ref
-      Email:Email
-      Name:Name
-      TelegramId:Text?
-      Phone:Phone?
-      Image:Image?
-      ChangeTimestamp:ChangeTimestamp
-
-    Cars - private
-      Id:Ref
-      PersonId:Ref
-      Model:Name
-      Capacity:Number?
-      Image:Image?
-      ChangeTimestamp:ChangeTimestamp
-
-    Trips
-      Id:Ref
-      CapacityMin:Number
-      CapacityMax:Number
-      Status:Enum - Offered | Settled | Canceled | Finished
-      ChangeTimestamp:ChangeTimestamp
-
-    TripCars
-      Id:Ref
-      TripId:Ref
-      CarId:Ref
-      ChangeTimestamp:ChangeTimestamp
-
-    TripPlaces
-      Id:Ref
-      TripId:Ref
-      PlaceId:Ref
-      DateTime:DateTime
-      // DateTimeReturn:DateTime
-      Price:Price - to return?
-      Agenda:Text
-      ChangeTimestamp:ChangeTimestamp
-
-    TripPersons
-      Id:Ref
-      TripId:Ref
-      PersonId:Ref
-      Role:Enum - Driver | Passenger
-      ChangeTimestamp:ChangeTimestamp
-
-    Wishes
-      Id:Ref
-      PersonId:Ref
-      PlaceId:Ref
-      Date:Date
-      TripId:Ref?
-      ChangeTimestamp:ChangeTimestamp
-
-    Feedback
-      Id:Ref
-      TripId:Ref
-      // PersonId:Ref
-      Stars:Number - 1..5
-      Text:Text?
-      ChangeTimestamp:ChangeTimestamp
-
-2022-04-24
+### 2022-04-24
 
     https://cloud.yandex.com/docs/functions/tutorials/telegram-bot-serverless
     https://cloud.yandex.com/en/docs/functions/tutorials/connect-to-ydb-nodejs
--- src/migrations.sql
+++ src/migrations.sql
@@ -10,23 +10,108 @@
 
   COMMIT;
 
+-- 2022-04-25
+
+  REPLACE INTO Countries
+    (id, name, updated)
+  VALUES
+    (0, 'Albania',              Datetime('2022-04-24T00:00:00Z')),
+    (1, 'Bosnia i Herzegovina', Datetime('2022-04-24T00:00:00Z')),
+    (2, 'Croatia',              Datetime('2022-04-24T00:00:00Z')),
+    (3, 'Serbia',               Datetime('2022-04-24T00:00:00Z'));
+
+  COMMIT;
+
 -- 2022-04-24
 
-  CREATE TABLE countries (
+  CREATE TABLE Cars (
+    capacity Uint8,
+    id Uint32,
+    model Utf8,
+    personId Uint32,
+    updated Datetime,
+    PRIMARY KEY (id)
+  );
+
+  CREATE TABLE Countries (
     id Uint32,
     name Utf8,
     updated Datetime,
     PRIMARY KEY (id)
   );
 
-  COMMIT;
+  CREATE TABLE Feedbacks (
+    id Uint32,
+    personId Uint32,
+    stars Uint8,
+    tripId Uint32,
+    updated Datetime,
+    PRIMARY KEY (id)
+  );
 
-  REPLACE INTO countries
-    (id, name, updated)
-  VALUES
-    (0, 'Албания', Datetime('2022-04-24T00:00:00Z')),
-    (1, 'Босния и Герцеговина', Datetime('2022-04-24T00:00:00Z')),
-    (2, 'Сербия', Datetime('2022-04-24T00:00:00Z')),
-    (3, 'Хорватия', Datetime('2022-04-24T00:00:00Z'));
+  CREATE TABLE Persons (
+    id Uint32,
+    tgId Uint64,
+    tgUsername String,
+    updated Datetime,
+    PRIMARY KEY (id)
+  );
+
+  CREATE TABLE Places (
+    countryId Uint32,
+    id Uint32,
+    name Utf8,
+    updated Datetime,
+    PRIMARY KEY (id)
+  );
+
+  CREATE TABLE Trips (
+    capacityMax Uint8,
+    capacityMin Uint8,
+    countryId Uint32,
+    id Uint32,
+    name Utf8,
+    status Uint8, -- Offered | Settled | Canceled | Finished
+    updated Datetime,
+    PRIMARY KEY (id)
+  );
+
+  CREATE TABLE TripCars (
+    carId Uint32,
+    id Uint32,
+    tripId Uint32,
+    updated Datetime,
+    PRIMARY KEY (id)
+  );
+
+  CREATE TABLE TripPlaces (
+    agenda Utf8,
+    datetime Datetime, -- datetimeReturn?
+    id Uint32,
+    placeId Uint32,
+    price Uint8, -- to return?
+    tripId Uint32,
+    updated Datetime,
+    PRIMARY KEY (id)
+  );
+
+  CREATE TABLE TripPersons (
+    id Uint32,
+    personId Uint32,
+    role Uint8, -- Driver | Passenger
+    tripId Uint32,
+    updated Datetime,
+    PRIMARY KEY (id)
+  );
+
+  CREATE TABLE Wishes (
+    date Date,
+    id Uint32,
+    personId Uint32,
+    placeId Uint32,
+    tripId Uint32,
+    updated Datetime,
+    PRIMARY KEY (id)
+  );
 
   COMMIT;
